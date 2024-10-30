ba8ea5efc28bf5072a6880254519b85205293e54
labiba-test <a.abuqoud@labiba.ai>
2024-10-30 14:52:46 +0300

    upload all files and new config

--- obj/Release/net8.0/Rep-test.AssemblyInfo.cs
+++ obj/Release/net8.0/Rep-test.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Rep-test")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73f551330d9fbcaa3da26ab61730d13e25e6513c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Rep-test")]
